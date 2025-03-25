b312f701432a9867b48d0a1e558bf240c01f4700
Eugen Sawitzki <e.sawitzki@reply.de>
2025-03-25 15:22:04 +0100

    install prettier and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import "dotenv/config";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello World!')
-})
+app.get("/", (c) => {
+  return c.text("Hello World!");
+});
 
 serve({
   fetch: app.fetch,
   port: 8080
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
