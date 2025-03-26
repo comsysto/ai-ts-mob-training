da82cce4492e504355853ca178e9f7d5f82ad84d
Eugen Sawitzki <e.sawitzki@reply.de>
2025-03-26 10:06:43 +0100

    execute prompt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import "dotenv/config";
+import { HTTPException } from "hono/http-exception";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
 const app = new Hono();
 
@@ -8,9 +11,24 @@ app.get("/", (c) => {
   return c.text("Hello World!");
 });
 
+app.get("/chat", async (c) => {
+  const inputPrompt = c.req.query("prompt");
+
+  if (inputPrompt === undefined) {
+    throw new HTTPException(422, { message: "Query parameter 'prompt' is required" });
+  }
+
+  const { text } = await generateText({
+    model: openai("gpt-4o"),
+    prompt: inputPrompt
+  });
+
+  return c.text(text);
+});
+
 serve({
   fetch: app.fetch,
-  port: 8080
+  port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`);
 });
